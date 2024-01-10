c1b4f51a592fabf5bfa0c75b0a4d91f683df7115
Imrancu <mdimran3772@gmail.com>
2024-01-11 00:42:38 +0600

    created contact route

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+import { getUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
-router.get('/get/:id', getListing)
+router.get('/get/:id', verifyToken, getListing)
+router.get('/:id', verifyToken, getUser)
 
 
 export default router;
