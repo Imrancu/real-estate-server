65dc25a1df921e0aa764a567ec1358439a274f2f
Imrancu <mdimran3772@gmail.com>
2024-01-11 16:58:48 +0600

    contact landlord route fixed the issue

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
-import { getUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -9,7 +8,6 @@ router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', verifyToken, getListing)
-router.get('/:id', verifyToken, getUser)
 
 
 export default router;
